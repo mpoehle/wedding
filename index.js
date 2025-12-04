d0ac8b526e713c5e9ad602584c8b8a431349f396
Michael Poehle <michael.poehle@gmail.com>
2025-12-04 16:51:51 +0100

    added countdown and background

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
-  $("#header").css({"background-position-y": 0})
-  $("#header > *").css({"opacity": 1})
+  $("#leaves").css({"top": 0})
+  $("#header").css({"opacity": 1})
   $("#content").css({"opacity": 1})
+  
+  wed_date = new Date(2026, 4, 19)
+  days_left = 1 + Math.floor((wed_date - new Date()) / (1000 * 60 * 60 * 24))
+  $("#countdown").html("just " + days_left + " days to go!")
 })
