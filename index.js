1177a4611db08d52a4cedebccbff27d87b4bc4ff
michael poehle <michael.poehle@emma-blossom.com>
2026-02-01 16:51:21 +0100

    more pics

--- index.js
+++ index.js
@@ -16,4 +16,25 @@ $(document).ready(() => {
       })
   })
   $("#content > div:not(#home)").hide()
+
+  $("#photos .pic:first").toggleClass("visible")
+
+  let timeout = null
+
+  const next_pic = () => {
+    const current = $("#photos .pic.visible")
+    let next = current.next().first()
+    if (!current.length || !next.length) {
+        next = $("#photos .pic:first")
+    }
+    current.toggleClass("visible")
+    next.toggleClass("visible")
+    if (timeout) {
+        clearTimeout(timeout)
+    }
+    timeout = setTimeout( next_pic, 4000)
+  }
+
+  timeout = setTimeout( next_pic, 4000)
+  $("#photos .pic").click(next_pic)
 })
